643375773932f6b7ff0ba474dafdf5c92ad4bf37
Unknown <jeffreyli@utexas.edu>
2017-01-09 21:17:25 -0800

    Initial commit to start up vlc

---
+++ CSharpMediaSelector/MediaSelector/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace MediaSelector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
