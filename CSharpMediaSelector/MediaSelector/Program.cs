4b04e74b88420b121ff8aed46f2f7e887d25db18
Unknown <jeffreyli@utexas.edu>
2017-01-09 21:23:30 -0800

    VLC now playing test video up with errors

--- CSharpMediaSelector/MediaSelector/Program.cs
+++ CSharpMediaSelector/MediaSelector/Program.cs
@@ -10,6 +10,7 @@ namespace MediaSelector
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.WindowStyle = ProcessWindowStyle.Hidden;
             startInfo.FileName = "C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe";
+            startInfo.Arguments = "vlc C:\\Users\\jeffrey.li\\Documents\\Git\\CSharpMediaSelector\\test.mp4";
             process.StartInfo = startInfo;
             process.Start();
         }
